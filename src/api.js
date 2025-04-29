444d6aff6a4a9d9bbea2ae4deb3b4c0774380c38
afonsoDomingos <karinganastudio23@gmail.com>
2025-04-29 19:16:27 +0200

    PA 10 - 29/04/2025: Atualizando a variável de producao

--- src/api.js
+++ src/api.js
@@ -4,16 +4,16 @@ import axios from 'axios';
 //const api = axios.create({
  // baseURL: 'http://localhost:5000/api',  // Endereço da sua API local
 //});
-
-//const api = axios.create({
-//  baseURL: 'https://apirpa.onrender.com/api',  // Endereço da sua API no Render
-//});
+//Usando a variável de para producao
+const api = axios.create({
+  baseURL: 'https://apirpa.onrender.com/api',  // Endereço da sua API no Render
+});
 
 
 // Usando a variável de ambiente para o baseURL
-const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,  // Obtém o valor da variável de ambiente
-});
+//const api = axios.create({
+ // baseURL: process.env.VUE_APP_API_URL,  // Obtém o valor da variável de ambiente
+//});
 
 
 export default api;
