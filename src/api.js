a4a9c717fbdedb9bdb65f562a209a1fce1e19d6e
afonsoDomingos <karinganastudio23@gmail.com>
2025-04-29 18:26:30 +0200

    PA 10 - 29/04/2025: Atualização da baseURL do Axios para usar variável de ambiente

--- src/api.js
+++ src/api.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
 // Aqui estamos configurando o endpoint da API
+//const api = axios.create({
+ // baseURL: 'http://localhost:5000/api',  // Endereço da sua API local
+//});
+
+//const api = axios.create({
+//  baseURL: 'https://apirpa.onrender.com/api',  // Endereço da sua API no Render
+//});
+
+
+// Usando a variável de ambiente para o baseURL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Endereço da sua API local
+  baseURL: process.env.VUE_APP_API_URL,  // Obtém o valor da variável de ambiente
 });
 
+
 export default api;
+
+import axios from 'axios';
+
+
+
