a1ca57e0ecaa03914d689faff98bb94b5686549d
afonsoDomingos <karinganastudio23@gmail.com>
2025-10-29 12:55:37 +0200

    RPA  V39 - 29/10/2025: Expondo o Host na Rede

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  server: {
+    host: '0.0.0.0', // expõe na rede
+    port: 3000,      // porta padrão (pode mudar se quiser)
   },
 });
