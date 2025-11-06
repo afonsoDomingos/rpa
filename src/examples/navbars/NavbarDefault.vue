80c4eed847696ab104e43d330d809d86f3b8b2d4
afonsoDomingos <karinganastudio23@gmail.com>
2025-11-06 23:53:19 +0200

    RPA  V39 - 06/11/2025: Mostrar a dashboard Link para Role Admin

--- src/examples/navbars/NavbarDefault.vue
+++ src/examples/navbars/NavbarDefault.vue
@@ -123,61 +123,37 @@ watch(() => type.value, (newValue) => {
 </script>
 
 <template>
-  <nav
-    class="navbar navbar-expand-lg top-0 position-fixed mx-auto my-3"
-    style="left: 0; right: 0; max-width: 1200px; border-radius: 12px; z-index: 1030;"
-    :class="{
+  <nav class="navbar navbar-expand-lg top-0 position-fixed mx-auto my-3"
+    style="left: 0; right: 0; max-width: 1200px; border-radius: 12px; z-index: 1030;" :class="{
       'shadow-none navbar-transparent': props.transparent,
       'blur shadow py-2': props.sticky,
       'navbar-light bg-white py-3': props.light,
       'navbar-dark bg-gradient-dark py-3': props.dark
-    }"
-  >
+    }">
     <div class="container px-0">
 
       <!-- Logo (Desktop e Mobile) com olho e efeito hover -->
-      <RouterLink
-        class="navbar-brand animated-logo d-flex align-items-center"
-        :to="{ name: 'presentation' }"
-        rel="tooltip"
-        title="Página inicial"
-        @mouseenter="hovering = true"
-        @mouseleave="hovering = false"
-      >
+      <RouterLink class="navbar-brand animated-logo d-flex align-items-center" :to="{ name: 'presentation' }"
+        rel="tooltip" title="Página inicial" @mouseenter="hovering = true" @mouseleave="hovering = false">
         <div class="logo-wrapper">
           <transition name="fade-scale">
             <olhodedeus v-if="!hovering" class="eye-logo" />
           </transition>
           <transition name="fade-scale">
-            <img
-              v-if="hovering"
-              src="@/assets/img/rPa.png"
-              alt="Logo Rpa"
-              class="brand-logo"
-            />
+            <img v-if="hovering" src="@/assets/img/rPa.png" alt="Logo Rpa" class="brand-logo" />
           </transition>
         </div>
       </RouterLink>
 
       <!-- Botão ASSINATURAS no mobile -->
-      <a
-        @click="$router.push({ name: 'Assinaturas' }); fecharDropdown()"
-        class="btn btn-sm mb-0 ms-auto d-lg-none d-block btn-assinatura"
-        role="button"
-      >
+      <a @click="$router.push({ name: 'Assinaturas' }); fecharDropdown()"
+        class="btn btn-sm mb-0 ms-auto d-lg-none d-block btn-assinatura" role="button">
         ASSINATURAS
       </a>
 
       <!-- Botão do menu -->
-      <button
-        class="navbar-toggler shadow-none ms-2"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navigation"
-        aria-controls="navigation"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
           <span class="navbar-toggler-bar bar2"></span>
@@ -185,77 +161,44 @@ watch(() => type.value, (newValue) => {
         </span>
       </button>
 
-      <div
-        class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0"
-        id="navigation"
-        ref="navbarCollapse"
-      >
+      <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation" ref="navbarCollapse">
         <ul class="navbar-nav navbar-nav-hover ms-auto ">
 
           <!-- Dropdown Páginas -->
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              role="button"
-              class="nav-link ps-2 d-flex cursor-pointer align-items-center borda-destacada"
-              :class="getTextColor()"
-              id="dropdownMenuPages"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <i
-                class="material-icons opacity-6 me-2 text-md"
-                :class="getTextColor()"
-              >
+            <a role="button" class="nav-link ps-2 d-flex cursor-pointer align-items-center borda-destacada"
+              :class="getTextColor()" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">
                 dashboard
               </i>
               Paginas
-              <img
-                :src="getArrowColor()"
-                alt="down-arrow"
-                class="arrow ms-2 d-lg-block d-none"
-              />
+              <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-2 d-lg-block d-none" />
             </a>
 
-            <div
-              class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
-              aria-labelledby="dropdownMenuPages"
-            >
+            <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
+              aria-labelledby="dropdownMenuPages">
               <div class="row">
                 <div class="col-12 px-4 py-2">
                   <div class="position-relative">
                     <div class="dropdown-header text-dark font-weight-bolder px-1">
                       Pagina Principal
                     </div>
-                    <RouterLink
-                      :to="{ name: 'about' }"
-                      class="dropdown-item border-radius-md"
-                      @click="fecharDropdown"
-                    >
+                    <RouterLink :to="{ name: 'about' }" class="dropdown-item border-radius-md" @click="fecharDropdown">
                       Sobre nós
                     </RouterLink>
-                    <RouterLink
-                      :to="{ name: 'contactus' }"
-                      class="dropdown-item border-radius-md"
-                      @click="fecharDropdown"
-                    >
+                    <RouterLink :to="{ name: 'contactus' }" class="dropdown-item border-radius-md"
+                      @click="fecharDropdown">
                       Contacte-nos
                     </RouterLink>
-                    <RouterLink
-                      :to="{ name: 'author' }"
-                      class="dropdown-item border-radius-md"
-                      @click="fecharDropdown"
-                    >
+                    <RouterLink :to="{ name: 'author' }" class="dropdown-item border-radius-md" @click="fecharDropdown">
                       Autor
                     </RouterLink>
 
                     <div class="dropdown-header text-dark font-weight-bolder px-0 mt-3">
                       Conta
                     </div>
-                    <RouterLink
-                      :to="{ name: 'signin-basic' }"
-                      class="dropdown-item border-radius-md"
-                      @click="fecharDropdown"
-                    >
+                    <RouterLink :to="{ name: 'signin-basic' }" class="dropdown-item border-radius-md"
+                      @click="fecharDropdown">
                       Inscrever-se
                     </RouterLink>
                   </div>
@@ -266,59 +209,43 @@ watch(() => type.value, (newValue) => {
 
           <!-- Usuário -->
           <li v-if="usuario" class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              role="button"
-              class="nav-link ps-2 d-flex cursor-pointer align-items-center"
-              :class="getTextColor()"
-              id="dropdownUser"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <i
-                class="material-icons opacity-6 me-2 text-md rotatable-profile"
-                :class="getTextColor()"
-              >person</i>
+            <a role="button" class="nav-link ps-2 d-flex cursor-pointer align-items-center" :class="getTextColor()"
+              id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="material-icons opacity-6 me-2 text-md rotatable-profile" :class="getTextColor()">person</i>
               {{ usuario?.nome || 'Usuário' }}
-              <img
-                :src="getArrowColor()"
-                alt="down-arrow"
-                class="arrow ms-2 d-lg-block d-none"
-              />
+              <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-2 d-lg-block d-none" />
             </a>
 
-            <div
-              ref="dropdownMenu"
+            <div ref="dropdownMenu"
               class="dropdown-menu dropdown-menu-end dropdown-menu-animation mt-0 p-2 borda-destacada"
-              aria-labelledby="dropdownUser"
-              style="min-width: 180px;"
-            >
-              <button
-                v-if="usuario && usuario.role?.toLowerCase() === 'admin'"
+              aria-labelledby="dropdownUser" style="min-width: 180px;">
+
+              <button v-if="usuario && usuario.role?.toLowerCase() === 'admin'"
                 class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'AdminAssinaturas' }); fecharDropdown();"
-              >
+                @click="$router.push({ name: 'dashboard' }); fecharDropdown();">
+                <i class="bi bi-speedometer2 text-warnin"></i> Dashboard
+              </button>
+
+
+              <button v-if="usuario && usuario.role?.toLowerCase() === 'admin'"
+                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'AdminAssinaturas' }); fecharDropdown();">
                 <i class="bi bi-wallet2 text-success"></i> Pagamentos
               </button>
 
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'MeusPagamentos' }); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'MeusPagamentos' }); fecharDropdown();">
                 <i class="bi bi-credit-card text-primary"></i> Meus Pagamentos
               </button>
 
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'MeusDocumentos' }); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'MeusDocumentos' }); fecharDropdown();">
                 <i class="bi bi-folder2-open text-info"></i> Meus Documentos
               </button>
 
               <div class="dropdown-divider"></div>
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2 text-danger"
-                @click="logout(); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2 text-danger"
+                @click="logout(); fecharDropdown();">
                 <i class="bi bi-box-arrow-right"></i> Sair
               </button>
             </div>
@@ -326,11 +253,7 @@ watch(() => type.value, (newValue) => {
 
           <!-- Login -->
           <li v-else class="nav-item dropdown dropdown-hover mx-2">
-            <router-link
-              to="/"
-              class="nav-link d-flex cursor-pointer align-items-center"
-              @click="fecharDropdown"
-            >
+            <router-link to="/" class="nav-link d-flex cursor-pointer align-items-center" @click="fecharDropdown">
               <i class="material-icons opacity-6 me-2 text-md">person</i>
               Entrar
             </router-link>
@@ -338,53 +261,33 @@ watch(() => type.value, (newValue) => {
 
           <!-- Ferramentas -->
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              role="button"
-              class="nav-link ps-2 d-flex cursor-pointer align-items-center"
-              :class="getTextColor()"
-              id="dropdownTools"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
+            <a role="button" class="nav-link ps-2 d-flex cursor-pointer align-items-center" :class="getTextColor()"
+              id="dropdownTools" data-bs-toggle="dropdown" aria-expanded="false">
               <i class="bi bi-gear-fill me-2 rotatable-icon"></i>
               Ferramentas
-              <img
-                :src="getArrowColor()"
-                alt="down-arrow"
-                class="arrow ms-2 d-lg-block d-none"
-              />
+              <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-2 d-lg-block d-none" />
             </a>
 
             <div
               class="dropdown-menu dropdown-menu-end dropdown-menu-animation mt-0 p-2 borda-destacada border-radius-lg"
-              aria-labelledby="dropdownTools"
-              style="min-width: 180px;"
-            >
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'ComunidadeRpa' }); fecharDropdown();"
-              >
+              aria-labelledby="dropdownTools" style="min-width: 180px;">
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'ComunidadeRpa' }); fecharDropdown();">
                 <i class="bi bi-people-fill text-info"></i> Comunidade
               </button>
 
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'CVGenerator' }); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'CVGenerator' }); fecharDropdown();">
                 <i class="bi bi-file-earmark-person text-warning"></i> Gerar CV
               </button>
 
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'GuardarDocumentos' }); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'GuardarDocumentos' }); fecharDropdown();">
                 <i class="bi bi-folder-plus text-success"></i> Guardar Docs
               </button>
 
-              <button
-                class="dropdown-item border-radius-md d-flex align-items-center gap-2"
-                @click="$router.push({ name: 'Viaturas' }); fecharDropdown();"
-              >
+              <button class="dropdown-item border-radius-md d-flex align-items-center gap-2"
+                @click="$router.push({ name: 'Viaturas' }); fecharDropdown();">
                 <i class="bi bi-car-front text-success"></i> Recuperar Viaturas
               </button>
             </div>
@@ -392,11 +295,8 @@ watch(() => type.value, (newValue) => {
 
           <!-- Assinatura Desktop -->
           <li class="d-none d-lg-block">
-            <a
-              class="btn btn-sm mb-0 ms-2 btn-assinatura"
-              role="button"
-              @click="$router.push({ name: 'Assinaturas' }); fecharDropdown();"
-            >
+            <a class="btn btn-sm mb-0 ms-2 btn-assinatura" role="button"
+              @click="$router.push({ name: 'Assinaturas' }); fecharDropdown();">
               Assinatura
             </a>
           </li>
@@ -427,14 +327,19 @@ watch(() => type.value, (newValue) => {
   vertical-align: middle;
 }
 
-.fade-scale-enter-active, .fade-scale-leave-active {
+.fade-scale-enter-active,
+.fade-scale-leave-active {
   transition: all 0.3s ease;
 }
-.fade-scale-enter-from, .fade-scale-leave-to {
+
+.fade-scale-enter-from,
+.fade-scale-leave-to {
   opacity: 0;
   transform: scale(0.8);
 }
-.fade-scale-enter-to, .fade-scale-leave-from {
+
+.fade-scale-enter-to,
+.fade-scale-leave-from {
   opacity: 1;
   transform: scale(1);
 }
@@ -489,14 +394,18 @@ watch(() => type.value, (newValue) => {
 
 /* Espaço para que o conteúdo não fique por baixo da navbar fixa */
 body {
-  padding-top: 60px; /* ajuste conforme a altura real do navbar */
+  padding-top: 60px;
+  /* ajuste conforme a altura real do navbar */
 }
 
 @media (max-width: 991px) {
   .navbar {
-    width: 90%; /* ocupa 90% da tela no mobile */
-    max-width: 380px; /* evita que fique muito larga em tablets */
-    margin: 0 auto; /* centraliza horizontalmente */
+    width: 90%;
+    /* ocupa 90% da tela no mobile */
+    max-width: 380px;
+    /* evita que fique muito larga em tablets */
+    margin: 0 auto;
+    /* centraliza horizontalmente */
     border-radius: 12px;
   }
 
@@ -505,6 +414,4 @@ body {
     padding-right: 10px;
   }
 }
-
 </style>
-
