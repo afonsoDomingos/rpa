1e154a35c5ad8754ada21bee0c0bf6f128ad464a
afonsoDomingos <karinganastudio23@gmail.com>
2025-11-07 00:40:20 +0200

    RPA  V39 - 07/11/2025: habilitando suporte PWA com vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,42 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'RecuperaAqui',
+        short_name: 'RPA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#800080',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
-    host: '0.0.0.0', // expõe na rede
-    port: 3000,      // porta padrão (pode mudar se quiser)
+    host: '0.0.0.0',
+    port: 3000,
   },
 });
