805e0a94e73ce27db49ee5d4ba3be300c68e5788
afonsoDomingos <karinganastudio23@gmail.com>
2025-11-14 16:17:43 +0200

    RPA  V40 - 16/11/2025: update 06

--- src/components/anunciantes/EditarAnuncio.vue
+++ src/components/anunciantes/EditarAnuncio.vue
@@ -247,7 +247,7 @@ const carregarAnuncios = async () => {
     const token = localStorage.getItem('token')
     if (!token) throw new Error('Token não encontrado')
 
-    const res = await api.get('/anuncios/meus', {
+    const res = await api.get('/meus', {
       headers: { Authorization: `Bearer ${token}` }
     })
 
