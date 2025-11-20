39ae2e7f5532f2018a920adf613480f35f6b10ff
afonsoDomingos <karinganastudio23@gmail.com>
2025-11-20 02:15:41 +0200

    RPA  V40 - 20/11/2025:Meta Pixel

--- src/components/anunciantes/AdCard.vue
+++ src/components/anunciantes/AdCard.vue
@@ -110,7 +110,7 @@ const ONE_HOUR_MS = 60 * 60 * 1000
 const fetchActiveAds = async () => {
   console.log('Buscando anúncios ativos...')
   try {
-    const res = await api.get('/ativos', { timeout: 10000 })
+    const res = await api.get('/anuncios/ativos', { timeout: 10000 })
     activeAds.value = (res.data || [])
       .filter(ad => ad.status === 'active' && ad.image && ad.name && ad.price >= 0 && ad._id)
       .slice(0, 10)
