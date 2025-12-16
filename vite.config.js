242d0e256f0dee48f4613c0edd0f74b6a1e96a45
afonsoDomingos <karinganastudio23@gmail.com>
2025-12-16 20:33:32 +0200

    feat: config gzip compression and caching headers

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
     vue(),
+    viteCompression(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
